1362626f734305946819b6b370138abecd54f4d7
chaitanyakalra <chaitanyakalra7@gmail.com>
2024-09-08 00:06:57 +0530

    fetching fixed  TT

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './screens/Home/home.jsx';
 import ConvictDetailsForm from './screens/ConvictDetails/ConvictDetailsForm';
-// import Login from './screens/Login/login.jsx';
+import Login from './screens/Login/login.jsx';
+import Botpress from './botfolder/botpress.jsx';
 
 function App() {
   return (
     <Router>
       <div className="App">
-
-        {/* <ConvictDetailsForm/> */}
-        <Home/>
         <Routes>
+          
           <Route path="/" element={<Home />} />
+
+          
           <Route path="/login" element={<Login />} />
+
+          
           <Route path="/accusedList" element={<Botpress />} />
-          <Route path="/convictDetails" element={<ConvictDetails />} />
+
+          
+          <Route path="/convictDetails" element={<ConvictDetailsForm />} />
         </Routes>
       </div>
-   </Router>
+    </Router>
   );
 }
 
