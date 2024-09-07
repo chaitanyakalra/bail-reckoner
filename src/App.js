2f0f447f315214624076b0e8a9eb079bb46487cc
sidkalla04 <141704442+sidkalla04@users.noreply.github.com>
2024-09-07 20:48:41 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './screens/Home/home.jsx';
-import Login from './screens/Login/login.jsx';
+import ConvictDetailsForm from './screens/ConvictDetails/ConvictDetailsForm';
+// import Login from './screens/Login/login.jsx';
 
 function App() {
   return (
     <Router>
       <div className="App">
+
+        {/* <ConvictDetailsForm/> */}
+        <Home/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
-    </Router>
+   </Router>
   );
 }
 
